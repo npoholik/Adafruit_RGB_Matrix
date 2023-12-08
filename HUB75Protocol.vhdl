b1dfddfa9506ed9ad72cf552048900e453e85129
npoholik <nick.poholik@slu.edu>
2023-12-07 20:10:29 -0600

    v2.0, Replaced Many Signals with Variables and one Main Process

--- HUB75Protocol.vhdl
+++ HUB75Protocol.vhdl
@@ -20,7 +20,7 @@ use IEEE.NUMERIC_STD.all;
 entity HUB75Protocol is
     port (clk : in std_logic;            -- Internal clock, Basys 3 clock ~100 MHz
           clk_out : out std_logic;        -- clock that is output to RGB matrix board
-          blank, latch : out std_logic;    -- blank (represents output enable OE) turns off display by being written high, latch (high->low) loads data into a row
+          blank, latch : buffer std_logic;    -- blank (represents output enable OE) turns off display by being written high, latch (high->low) loads data into a row
           A3, A2, A1, A0 : out std_logic; --  4 address values (determines which line to display out of 32) (A4 is unused as it is unncessary for 32x32 matrix)
           R0,G0,B0,R1,G1,B1 : out std_logic); -- color value (R0,G0,B0 for upper half of board; R1, G1, B1 for lower half of board)
 end HUB75Protocol;
@@ -39,17 +39,20 @@ architecture behav of HUB75Protocol is
 
 -- ***Define signals***
 ---------------------------------------------------------------------------------------------------
-    signal latchStatus, blankStatus : std_logic;   -- will allow determination of next step to take
-    signal latchIn, blankIn : std_logic := '0';    -- will determine what is going to be output onto matrix board (also means it can be read during process unlike output latch and blank)
+    --signal latchData: std_logic;   -- will allow determination of next step to take
+    --signal latchIn, blankIn : std_logic := '0';    -- will determine what is going to be output onto matrix board (also means it can be read during process unlike output latch and blank)
 
-    signal rowCount : std_logic_vector(3 downto 0) := "0000";      -- signal for row address, counts up to 15 (16 total rows)
-    signal colCount : std_logic_vector(4 downto 0) := "00000";      -- signal for current column, counts up to 31 (32 total columns)
+    --signal rowCount : unsigned(3 downto 0) := "0000";      -- signal for row address, counts up to 15 (16 total rows)
+    --signal colCount : unsigned(4 downto 0) := "00000";      -- signal for current column, counts up to 31 (32 total columns)
 
-    signal rgb : std_logic_vector(2 downto 0) := "001";            -- signal for color values of an individual pixel (initial value: blue)
-    signal rgbCounter : integer := 1;             -- will determine when to change RGB values
+    --signal clockCol : std_logic := '0';
 
-    signal count : integer := 1;                -- Count signals for clock divider/pulse width
-    signal clk_div : std_logic;                 -- 60 kHz clock to use for outputting to board as well as in main process
+    --signal rgb : unsigned(2 downto 0) := "001";            -- signal for color values of an individual pixel (initial value: blue)
+
+    --signal count : integer := 1;                -- Count signals for clock divider/pulse width
+    --signal clk_div : std_logic;                 -- 60 kHz clock to use for outputting to board as well as in main process
+    
+    
 ----------------------------------------------------------------------------------------------------
 
 -- ***Architecture begin***
@@ -61,91 +64,144 @@ begin
     -- latchIn and blankIN helps know internally what value will be getting output to the board
     -- ***ALTERNATIVELY, MAKE LATCH AND BLANK BUFFER SIGNALS IN ENTITY DECLARATION ***
     -- ^^^ would honestly be easier, it didn't occur to me when initially starting
-    latch <= latchIn;
-    blank <= blankIn;
+    --latch <= latchIn;
+    --blank <= blankIn;
 
-    -- Send color data to the RGB pins
-    R0 <= rgb(2); G0 <= rgb(1); B0 <= rgb(0);
-    R1 <= rgb(2); G1 <= rgb(1); B1 <= rgb(0);
+    
+    --clk_out <= clk_div; -- clock the data into the columns
+    
+
+    
+
     ------------------------------------------------------------------------------------------------
 
 
 
     -- ***Sequential Blocks: ***
     ------------------------------------------------------------------------------------------------
-    Main: process(clk_div)
-        begin
-            if rising_edge(clk_div) then
-                clk_out <= '1';
-                ----------------------------------------------------------------------------------------------------------------------------------------
-                if blankIn = '0' then   -- If blank is not set, then line is active (MUST NOW MOVE TO NEXT ROW)
+    Main: process(clk)
+     --*** Variable declarations: ***
+     variable rowCount : unsigned(3 downto 0) := "0000";   --signal for row address, counts up to 15 (16 total rows)
+     variable colCount : unsigned(4 downto 0) := "00000";  -- signal for current column, counts up to 31 (32 total columns)
+     variable rgb : unsigned(2 downto 0) := "000";         -- signal for color values of an individual pixel (initial value: black)
+     variable cycleRGB : integer := 1;                     -- will determine when to change RGB values
+     variable willLatchData : std_logic := '0';            -- will let process know when to get ready to latch (high -> signal to board)
 
-                    -- if blankStatus is not set, then update the address to the board from rowCount(previous iteration through should have incremented row address) and set blankStatus for next iteration
-                    if blankStatus = '0' then 
-                        A3 <= rowCount(3);
-                        A2 <= rowCount(2);
-                        A1 <= rowCount(1);
-                        A0 <= rowCount(0);
-                        blankStatus <= '1';
-                    -- if blankStatus is set, then the current row selected will be turned off by writing a 1 to blankIn (which gets output to the board via blank)
-                    else 
-                        blankStatus <= '0';
-                        blankIn <= '1';
-                    end if;
-                ----------------------------------------------------------------------------------------------------------------------------------------  
-                else  -- else blank is set, and line is not active (updating colors will now take place)
-
-                    -- Indicates all data is loaded into the columns, and row is ready to be latch (low to high)
-                    if colCount = "11111" then 
-                        colCount <= "00000";
-                        latchStatus <= '1';
-                    -- Indicates that the latch will be written high to prepare the buffer to transfer
-                    elsif latchStatus = '1' then
-                        latchStatus <= '0';
-                        latchIn <= '1';
-                    -- Indicates that the latch low to high will now take place
-                    elsif latchIn = '1' then
-                        latchIn <= '0'; -- High to low for latch, data loaded into row
-                        blankIn <= '0'; -- Row is turned back on by clearing blank
-                        rowCount <= std_logic_vector(unsigned(row) + 1); -- Row address is incremented to the next
-                    -- If nothing else is set, then continue incrementing through the columns
-                    else 
-                        colCount <= std_logic_vector(unsigned(colCount) + 1);
-                    end if;
-
-                end if;      
-                ----------------------------------------------------------------------------------------------------------------------------------------
-            else 
-                clk_out <= '0';
-            end if;
-    end process;
-
-
-    -- *** PROCESS TO OUTPUT 60 KHZ CLOCK ***
-    ClockDivider: process(clk) 
-        variable temp : std_logic := '0';
+     --*** PROCESS BEGIN ***               
         begin
             if rising_edge(clk) then
-                count <= count + 1;
-                if count = 833  then  -- 100 MHz clock takes prescalar of ~1667 to divide it into 60 kHz, so ~833 for 50% duty cycle between time high and time low
-                    clk_div <= not temp; 
-                    count <= 1;
+                -- *** LOGIC TO HANDLE REFRESHING: ***
+                ----------------------------------------------------------------------------------------------------------------------------------------
+                if blank = '0' then   -- If blank is not set, then line is active (TURN OFF NEXT ROW)
+                     blank <= '1'; -- asserting blank high turns off row
+                     latch <= '0'; -- assert latch low
+                     clk <= '0';   -- do not send clock 
+                
+                elsif blank = '1' then  -- else blank is set, and line is not active (updating colors will now take place)
+
+                    clk_out <= clk; -- clock data into each column by sending out clock 
+                    -- Send in current RGB Data to board
+                    R0 <= rgb(2); G0 <= rgb(1); B0 <= rgb(0);
+                    R1 <= rgb(2); G1 <= rgb(1); B1 <= rgb(0);
+                    -- Indicates all data is loaded into the columns, and row is ready to be latch (low to high)
+                    if colCount = "11111" then 
+                        if latchData = '0' then
+                            latch <= '1'; 
+                            willLatchData := '1'; -- Indicates that the latch will be written high to prepare the buffer to transfer
+                        elsif willLatchData = '1' then
+                            latch <= '0'; -- Buffer toggled from high to low loads data into the row
+                            blank <= '0'; 
+                            rowCount := rowCount + 1; -- Row address is incremented to the next
+                            -- **Update Address value out to board
+                            A3 <= std_logic(rowCount(3));
+                            A2 <= std_logic(rowCount(2));
+                            A1 <= std_logic(rowCount(1));
+                            A0 <= std_logic(rowCount(0));
+                            colCount := "00000";
+                        end if;
+                    else 
+                         colCount <= colCount + 1; -- move to next column
+                    end if;
+                else 
+                    blank <= '0';
+                    latchData <= '0';
+                end if;   
+                
+                --*** LOGIC TO SHIFT RGB VALUE ***
+                cycleRGB := cycleRGB + 1;
+                if cycleRGB = 80000 then
+                    cycleRGB := 1;
+                    rgb := rgb + 1;
                 end if;
+                ----------------------------------------------------------------------------------------------------------------------------------------
+
             end if;
     end process;
+
+
+
+
+
+
+
+
+--   process(colCount)
+--   variable temp : std_logic := '0';
+ --   begin
+--         clk_out <= not temp;
+--         if colCount = "11111" then
+--            R0 <= rgb(2); G0 <= rgb(1); B0 <= rgb(0);
+--            R1 <= rgb(2); G1 <= rgb(1); B1 <= rgb(0);
+--        end if;
+ --   end process;
 
 
     -- *** PROCESS TO UPDATE RGB VALUES ***
-    UpdateRGB: process(rowCount, colCount) 
-    begin 
-        if rowCounter = "1111" and colCount = "11111" then
-            rgbCounter <= rgbCounter + 1;
-            if rgbCounter = 801 then -- Looking for ~800 refreshes of the entire board 
-                rgb <= std_logic_vector(unsigned(rgb) + 1);
-                rgbCounter <= 1;
-            end if;
-        end if;
-    end process;
+--    UpdateRGB: process(clk, rowCount, colCount)
+--        variable rgbCounter : integer := 1;  -- will determine when to change RGB values
+--    begin 
+--    if rising_edge(clk) then
+--       if rowCount = "1111" and colCount = "11111" then
+--            rgbCounter := rgbCounter + 1;
+--            if rgbCounter = 1000000 then -- Looking for ~80,000 refreshes of the entire board 
+--                rgbCounter := 1;
+--                rgb <= rgb + 1;
+--            end if;
+--        end if;
+--    end if;
+--    end process; 
 
+
+--    process(clk, rgb)
+--    begin
+--    if rising_edge(clk)then
+--        R0 <= rgb(2); G0 <= rgb(1); B0 <= rgb(0);
+--        R1 <= rgb(2); G1 <= rgb(1); B1 <= rgb(0);
+ --       end if;
+--    end process;
+
+
+   -- *** PROCESS TO OUTPUT 25 MHz CLOCK ***
+--    ClockDivider: process(clk) 
+--        variable temp : std_logic := '0';
+--        variable count : integer := 1;
+--        begin
+--            if rising_edge(clk) then
+--                count := count + 1;
+--                if count = 4  then  -- 100 MHz clock takes prescalar of 
+--                   clk_div <= not temp; 
+--                   count := 1;
+--                end if;
+--            end if;
+--    end process;
+    
+    
+      --  process(colCount) 
+ --  variable temp : std_logic := '0';
+ --   begin
+ --       clk_out <= not temp;
+ --   end process;
+ 
     ------------------------------------------------------------------------------------------------
 end behav;
+
