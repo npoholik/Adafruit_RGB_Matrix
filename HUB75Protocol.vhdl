497731a7048562a709dafc632ac36daacdeb0ada
Nick P <nick.poholik@slu.edu>
2023-12-08 12:37:32 -0600

    v12.8.23.1 Added Documentation, Edited variable RGB. Requires Further Testing

--- HUB75Protocol.vhdl
+++ HUB75Protocol.vhdl
@@ -18,10 +18,10 @@ use IEEE.STD_LOGIC_UNSIGNED.all;
 use IEEE.NUMERIC_STD.all;
 
 entity HUB75Protocol is
-    port (clk : in std_logic;            -- Internal clock, Basys 3 clock ~100 MHz
-          clk_out : out std_logic;        -- clock that is output to RGB matrix board
+    port (clk : in std_logic;                 -- Internal clock, Basys 3 clock ~100 MHz
+          clk_out : out std_logic;            -- clock that is output to RGB matrix board
           blank, latch : buffer std_logic;    -- blank (represents output enable OE) turns off display by being written high, latch (high->low) loads data into a row
-          A3, A2, A1, A0 : out std_logic; --  4 address values (determines which line to display out of 32) (A4 is unused as it is unncessary for 32x32 matrix)
+          A3, A2, A1, A0 : out std_logic;     --  4 address values (determines which line to display out of 32) (A4 is unused as it is unncessary for 32x32 matrix)
           R0,G0,B0,R1,G1,B1 : out std_logic); -- color value (R0,G0,B0 for upper half of board; R1, G1, B1 for lower half of board)
 end HUB75Protocol;
 
@@ -37,7 +37,8 @@ architecture behav of HUB75Protocol is
 -- **** CLOCK OF 60 kHZ for 32x32 MATRIX *****
 -- *******************************************************************************************************************
 
--- ***Define signals***
+
+-- ***Define signals*** (Most signals moved to Main process)
 ---------------------------------------------------------------------------------------------------
     --signal latchData: std_logic;   -- will allow determination of next step to take
     --signal latchIn, blankIn : std_logic := '0';    -- will determine what is going to be output onto matrix board (also means it can be read during process unlike output latch and blank)
@@ -55,10 +56,11 @@ architecture behav of HUB75Protocol is
     signal willLatchData, willSetBlank : std_logic := '0'; -- will let process know when to get ready to latch (high -> signal to board)
 ----------------------------------------------------------------------------------------------------
 
+
 -- ***Architecture begin***
 begin
 
-    -- *** Concurrent Blocks: ***
+    -- *** Concurrent Blocks: *** (Unused in current iteration)
     ------------------------------------------------------------------------------------------------
     -- Internal signal to output signal assignments
     -- latchIn and blankIN helps know internally what value will be getting output to the board
@@ -70,9 +72,6 @@ begin
     
     --clk_out <= clk_div; -- clock the data into the columns
     
-
-    
-
     ------------------------------------------------------------------------------------------------
 
 
@@ -83,8 +82,8 @@ begin
      --*** Variable declarations: ***
      variable rowCount : unsigned(3 downto 0) := "0000";   --signal for row address, counts up to 15 (16 total rows)
      variable colCount : unsigned(4 downto 0) := "00000";  -- signal for current column, counts up to 31 (32 total columns)
-     variable rgb : unsigned(2 downto 0) := "000";         -- signal for color values of an individual pixel (initial value: black)
      variable cycleRGB : integer := 1;                     -- will determine when to change RGB values
+     variable rgb : std_logic_vector(2 downto 0) := "000";         -- signal for color values of an individual pixel (initial value: black)   
 
      --*** PROCESS BEGIN ***               
         begin
@@ -96,6 +95,7 @@ begin
                 -- *** LOGIC TO HANDLE REFRESHING: ***
                 ----------------------------------------------------------------------------------------------------------------------------------------
                 if blank = '0' then   -- If blank is not set, then line is active (TURN OFF NEXT ROW)
+                     -- Following if/else will allow a couple of clock cycles to account for propagation delay 
                      if willSetBlank = '0' then
                         willSetBlank <= '1';
                      else 
@@ -115,25 +115,25 @@ begin
                         
                         --*** LOGIC TO SHIFT RGB VALUE ***
                         cycleRGB := cycleRGB + 1;
-                            if cycleRGB = 100000 then
+                            if cycleRGB = 800 then -- Looking for 50 cycles of all columns being refreshed
                                 cycleRGB := 1;
-                                rgb := rgb + 1;
+                                rgb := std_logic_vector(unsigned(rgb) + 1); -- increment color value by 1 (entire pattern is 8 including off)
                             end if;
                             
-                            
+                    -- The following elsif's handle latching data into the row before moving onto the next
                     elsif willLatchData = '1' then
                         willLatchData <= '0';
                         latch <= '1';
                     elsif latch = '1' then 
-                        latch <= '0'; 
-                        blank <= '0';
-                        rowCount := rowCount + 1;
+                        latch <= '0'; -- reset latch
+                        blank <= '0'; -- turn current row on
+                        rowCount := rowCount + 1; -- move to next row 
                         A3 <= rowCount(3);
                         A2 <= rowCount(2);
                         A1 <= rowCount(1);
                         A0 <= rowCount(0);
                     else 
-                         colCount := colCount + 1; -- move to next column
+                         colCount := colCount + 1; -- Move to the next column if still possible
                     end if;
                     
                 end if;   
@@ -143,10 +143,8 @@ begin
     end process;
 
 
-
-
-
-
+    -- *** OLD ITERATIONS: INCORRECT/UNUSED
+    ---------------------------------------------------------------------------------------------------------------------------
 
 
 --   process(colCount)
@@ -205,6 +203,6 @@ begin
  --   begin
  --       clk_out <= not temp;
  --   end process;
- 
-    ------------------------------------------------------------------------------------------------
+    ------------------------------------------------------------------------------------------------------
+
 end behav;
